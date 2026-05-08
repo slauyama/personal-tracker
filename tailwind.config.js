15237a4622ef21b03fc8ff411f8bec1c8ed728ad
slauyama <7174954+slauyama@users.noreply.github.com>
2026-05-08 09:22:04 -0700

    Add dark mode check and use non standard fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Futura', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
